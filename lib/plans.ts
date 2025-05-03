819429df8c1d3c571d539da8d5131c8fbadb84b1
aliamerj <aliamer19ali@gmail.com>
2025-05-03 13:36:45 +0300

    change price

--- lib/plans.ts
+++ lib/plans.ts
@@ -10,25 +10,25 @@ export const Plans ={
     connections: 10,
     pages: 50,
     retrievals: 250,
-    price: 24.99
+    price: 14.99
   },
   PRO: {
     connections:Infinity,
     pages: 100,
     retrievals: 500,
-    price: 49.99
+    price: 29.99
   },
   BUSINESS: {
     connections: Infinity,
     pages: 5_000,
     retrievals: 10_000,
-    price: 99.99,
+    price: 59.99,
   },
   ENTERPRISE: {
     connections: Infinity,
     pages: 10_000,
     retrievals: Infinity,
-    price: 499.99,
+    price: 399.99,
   },
   OS: {
     connections: Infinity,
